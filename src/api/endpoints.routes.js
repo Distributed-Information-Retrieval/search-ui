08b40a63e06d8ad2ce5da381cb3a49f410a8076f
jotacib3 <jotacib3@gmail.com>
2022-07-05 04:41:48 -0400

    chore: add enpoints.routes.js

---
+++ src/api/endpoints.routes.js
@@ -0,0 +1,9 @@
+const API_HOST_1 = process.env.REACT_APP_TRAINING_API_HOST_1;
+const API_HOST_2 = process.env.REACT_APP_TRAINING_API_HOST_2;
+const API_HOST_3 = process.env.REACT_APP_TRAINING_API_HOST_3;
+
+export {
+  API_HOST_1,
+  API_HOST_2,
+  API_HOST_3,
+};
